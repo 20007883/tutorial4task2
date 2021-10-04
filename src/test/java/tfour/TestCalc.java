20060be20a382f1850bcd442f089db3358897c77
20007883 <90370323+20007883@users.noreply.github.com>
2021-10-04 19:07:26 +0800

    Add files via upload

--- src/test/java/tfour/TestCalc.java
+++ src/test/java/tfour/TestCalc.java
@@ -1,7 +1,5 @@
 package tfour;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalc {
@@ -14,7 +12,7 @@ public class TestCalc {
     @Test
     public  void  teststruct(){
         Calc a=new Calc();
-        assertEquals(a.sub(1,2),3);
+        assertEquals(a.sub(9,8),1);
 
     }
 }
